39e47abc9ad05fc7dc62ee70e1661ca455f81151
Viskytrn <gytongavisky@gmail.com>
2025-02-04 13:48:24 +0300

    Initial Commit

---
+++ Introduction.py
@@ -0,0 +1,7 @@
+#Print is a method used to display a value
+
+
+print("Enoch")
+print("emobilis")
+print(100)
+print(10+20)
---
+++ Variable.py
@@ -0,0 +1,20 @@
+#Variable is a container used to store a value
+
+location ="Westlands"
+firstname ="Enoch"
+lastname ="Bitok"
+school ="emobilis"
+
+x =39
+y =2
+z =78
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+print(firstname,lastname)
+print("My full name is",firstname,lastname)
+print(school,"is a coding school")
+print(x+y)
+print("The product of ",x,"and",y,"is",z)

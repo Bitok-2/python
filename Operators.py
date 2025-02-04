c3a4c087c3ae9cf143fe8743b31ff4e21629a351
Bitok-2 <bitokenock38@gmail.com>
2025-02-04 16:14:10 +0300

    Initial Commit

---
+++ Operators.py
@@ -0,0 +1,29 @@
+#>Arithmetic operators
+x= 10
+y= 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y) #Modulus returns the remainder
+
+#comparison operators
+print(x< y)
+print(x > y)
+print(x == y) #Equal to
+print(x != y) #Not equal to
+
+#Assignment operators
+a = 30
+print(a)
+a -= 2
+print(a)
+
+number = (45 - 2 *3 +5)
+print(number)
+
+#Logical operators - and, or, not
+print(55 < 234 and 67 < 23)
+print(55 < 234 or 67 < 23)
+print(not(55 < 234 or 67 < 23))
---
+++ datatype.py
@@ -0,0 +1,29 @@
+number = 50 #Int
+num = 56.98 #Float
+greeting = "Hello there" #Str
+hasfeathers = True #Boolean
+
+#Data structures
+cars = ["Mercedes","Toyota","BMW"] #List
+fruits = ("Banana","Orange","Apple")#Tuple
+countries = {"England","France","Germany"}
+student = {
+    "firstname" : "Ann",
+    "course" : "MIT",
+    "age" : 19,
+    "nationality" : "Kenyan",
+}
+
+print(number)
+print(num)
+print(greeting)
+print(hasfeathers)
+print(cars)
+print(fruits)
+print(countries)
+print(student)
+print(student["nationality"])
+
+#Typecasting - Converting one data type to another
+print(float(number))
+print(int(num))
